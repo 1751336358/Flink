d4e81f95734caf287644b3f696d0b9d44cfaa6f2
liuli <liuli@daling.com>
2019-09-15 12:20:12 +0800

    DataSet 表的关联

---
+++ src/main/java/com/flink/DataSetJob.java
@@ -0,0 +1,112 @@
+package com.flink;
+
+import org.apache.flink.api.common.functions.CrossFunction;
+import org.apache.flink.api.common.functions.JoinFunction;
+import org.apache.flink.api.common.operators.Order;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.api.java.tuple.Tuple2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataSetJob {
+    public static void main(String[] args) throws Exception {
+        sort();
+    }
+
+    public static void sort() throws Exception{
+        List<Tuple2<Integer,Integer>> list1 = new ArrayList<>();
+        list1.add(new Tuple2<>(3,3));
+        list1.add(new Tuple2<>(4,4));
+        list1.add(new Tuple2<>(1,1));
+        list1.add(new Tuple2<>(2,2));
+        list1.add(new Tuple2<>(2,3));
+        list1.add(new Tuple2<>(2,1));
+        ExecutionEnvironment env = getExecutionEnvironment();
+
+        env.fromCollection(list1).sortPartition(0, Order.DESCENDING).sortPartition(1,Order.ASCENDING).print();
+    }
+
+    public static void union() throws Exception{
+        List<Tuple2<String,String>> list1 = new ArrayList<>();
+        list1.add(new Tuple2<>("aaa","111"));
+        list1.add(new Tuple2<>("bbb","222"));
+        list1.add(new Tuple2<>("ccc","333"));
+        list1.add(new Tuple2<>("ddd","444"));
+        List<Tuple2<String,String>> list2 = new ArrayList<>();
+        list2.add(new Tuple2<>("eee","555"));
+        list2.add(new Tuple2<>("fff","666"));
+        list2.add(new Tuple2<>("ggg","777"));
+        ExecutionEnvironment env = getExecutionEnvironment();
+
+        env.fromCollection(list1).union(env.fromCollection(list2)).print();
+    }
+    public static void cross() throws Exception{
+        List<Tuple2<String,String>> list1 = new ArrayList<>();
+        list1.add(new Tuple2<>("aaa","111"));
+        list1.add(new Tuple2<>("bbb","222"));
+        list1.add(new Tuple2<>("ccc","333"));
+        list1.add(new Tuple2<>("ddd","444"));
+        List<Tuple2<String,String>> list2 = new ArrayList<>();
+        list2.add(new Tuple2<>("eee","555"));
+        list2.add(new Tuple2<>("fff","666"));
+        list2.add(new Tuple2<>("ggg","777"));
+
+        ExecutionEnvironment env = getExecutionEnvironment();
+        //cross，笛卡尔积
+        env.fromCollection(list1).cross(env.fromCollection(list2)).with(new CrossFunction<Tuple2<String,String>, Tuple2<String,String>, Object>() {
+            @Override
+            public Tuple2<String, String> cross(Tuple2<String, String> left, Tuple2<String, String> right) throws Exception {
+                return new Tuple2<String, String>(left.getField(0)+"|"+right.getField(0),left.getField(1)+"|"+right.getField(1));
+            }
+        }).print();
+    }
+    //leftOuterJoin与rightOuterJoin类似,不做演示
+    public static void leftOuterJoin() throws Exception{
+        List<Tuple2<String,String>> list1 = new ArrayList<>();
+        list1.add(new Tuple2<>("aaa","111"));
+        list1.add(new Tuple2<>("bbb","222"));
+        list1.add(new Tuple2<>("ccc","333"));
+        list1.add(new Tuple2<>("ddd","444"));
+        List<Tuple2<String,String>> list2 = new ArrayList<>();
+        list2.add(new Tuple2<>("ccc","xxx"));
+        list2.add(new Tuple2<>("fff","666"));
+        list2.add(new Tuple2<>("ggg","777"));
+
+        ExecutionEnvironment env = getExecutionEnvironment();
+        //leftOuterJoin<==>left join
+        env.fromCollection(list1).leftOuterJoin(env.fromCollection(list2)).where(0).equalTo(0).with(new JoinFunction<Tuple2<String,String>, Tuple2<String,String>, Object>() {
+            @Override
+            public Tuple2<String, String> join(Tuple2<String, String> left, Tuple2<String, String> right) throws Exception {
+                if(right == null){
+                    return left;
+                }
+                return right;
+            }
+        }).print();
+    }
+    public static void join() throws Exception{
+        List<Tuple2<String,String>> list1 = new ArrayList<>();
+        list1.add(new Tuple2<>("aaa","111"));
+        list1.add(new Tuple2<>("bbb","222"));
+        list1.add(new Tuple2<>("ccc","333"));
+        List<Tuple2<String,String>> list2 = new ArrayList<>();
+        list2.add(new Tuple2<>("ddd","444"));
+        list2.add(new Tuple2<>("eee","555"));
+        list2.add(new Tuple2<>("aaa","666"));
+
+        ExecutionEnvironment env1 = getExecutionEnvironment();
+        DataSource ds1 = env1.fromCollection(list1);
+        DataSource ds2 = env1.fromCollection(list2);
+        ds1.join(ds2).where(0).equalTo(0).print();
+    }
+    public static void distinct() throws Exception{
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        env.fromElements(111,222,333,444,555,111,222).distinct().print();
+    }
+
+    private static ExecutionEnvironment getExecutionEnvironment(){
+        return ExecutionEnvironment.getExecutionEnvironment();
+    }
+}
--- src/main/java/com/flink/TableJob.java
+++ src/main/java/com/flink/TableJob.java
@@ -1,20 +1,9 @@
 package com.flink;
 
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.table.api.TableEnvironment;
-import org.apache.flink.table.api.java.StreamTableEnvironment;
-
 public class TableJob {
-    public static void main(String[] args) throws Exception {
+    /*public static void main(String[] args) throws Exception {
 
-        testTable_01();
-    }
 
-    public static void testTable_01() throws Exception{
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+    }*/
 
-        StreamTableEnvironment tableEnv = TableEnvironment.getTableEnvironment(env);
-        
-        env.execute("execute");
-    }
 }
