51486a7a6f90fa24d487e84139c930410418ca09
liuli <liuli@daling.com>
2019-09-29 21:47:55 +0800

    从kafka读取数据，写入rabbitmq

--- src/main/java/kafka/_01_kafka_demo/KafkaSinkStreamingJob_01.java
+++ src/main/java/kafka/_01_kafka_demo/KafkaSinkStreamingJob_01.java
@@ -6,11 +6,11 @@ import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
+import utils.KafkaUtils;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.util.Properties;
 
 /**
  * 自定义DataSource 和DataSink
@@ -23,26 +23,19 @@ public class KafkaSinkStreamingJob_01 {
 
 	public static void main(String[] args) throws Exception {
 		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-		readFromKafka(env);
+		execute(env);
 		env.execute("execute");
 
 	}
 
 	/**
-	 * 从lafka读取数据
+	 * 从kafka读取数据写入mysql
 	 * @param env
 	 */
-	public static void readFromKafka(StreamExecutionEnvironment env){
-		Properties props = new Properties();
-		props.put("bootstrap.servers", "192.168.234.130:9092");
-		props.put("zookeeper.connect", "192.168.234.130:2181");
-		props.put("group.id", "metric-group");
-		props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-		props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-		props.put("auto.offset.reset", "latest");	//earliest,latest,和none
+	public static void execute(StreamExecutionEnvironment env){
 
 		//flink自带的从kafka读入工具,(topic,字符解码器,属性文件)
-		DataStreamSource<String> ds = env.addSource(new FlinkKafkaConsumer011<String>("test", new SimpleStringSchema(), props)).setParallelism(1);
+		DataStreamSource<String> ds = env.addSource(new FlinkKafkaConsumer011<String>("test", new SimpleStringSchema(), KafkaUtils.getKfkPreperties())).setParallelism(1);
 		ds.addSink(new Sink2MySQL_01());
 	}
 
---
+++ src/main/java/kafka/_05kafka2Rabbitmq/Kafka2Rabbitmq.java
@@ -0,0 +1,44 @@
+package kafka._05kafka2Rabbitmq;
+
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
+import org.apache.flink.streaming.connectors.rabbitmq.RMQSink;
+import org.apache.flink.streaming.connectors.rabbitmq.common.RMQConnectionConfig;
+import utils.KafkaUtils;
+
+/**
+ * 从kafka读取数据，写入rabbitmq
+ * 压力造数据命令
+ * ./kafka-producer-perf-test.sh  --topic test --num-records 100000000 --record-size 687  --producer-props   bootstrap.servers=192.168.234.130:9092  batch.size=10000   --throughput 30000
+ */
+public class Kafka2Rabbitmq {
+
+
+	public static void main(String[] args) throws Exception {
+		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+		rmq(env);
+		env.execute("execute");
+
+	}
+
+    /**
+     * 从kafka读取数据写入rabbitmq
+	 * @param env
+	 */
+	public static void rmq(StreamExecutionEnvironment env){
+		DataStreamSource<String> ds = env.addSource(new FlinkKafkaConsumer011<String>("test", new SimpleStringSchema(), KafkaUtils.getKfkPreperties())).setParallelism(1);
+			RMQConnectionConfig config = new RMQConnectionConfig.Builder()
+					.setHost("")
+					.setVirtualHost("")
+					.setPort(5672)
+					.setUserName("")
+					.setPassword("")
+					.build();
+
+		ds.addSink(new RMQSink<String>(config,"",new SimpleStringSchema()));
+
+	}
+
+}
